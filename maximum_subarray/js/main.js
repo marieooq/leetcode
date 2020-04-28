8dc067f1f9b40dbf65719f5598520ddef99db92d
Marie Otaki <otakimarie@gmail.com>
2020-04-28 12:22:49 -0700

    modified maximum_subarray

--- maximum_subarray/js/main.js
+++ maximum_subarray/js/main.js
@@ -5,6 +5,9 @@
 // Input: [-2,1,-3,4,-1,2,1,-5,4],
 // Output: 6
 // Explanation: [4,-1,2,1] has the largest sum = 6.
+
+//reference: https://github.com/iponkan/leetcode-js/blob/master/problems/53.maximum-sum-subarray-en.md
+
 const helper = (list, m, n) => {
   if (m === n) return list[m];
   let sum = 0;
