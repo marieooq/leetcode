2f0e8d18f0d1d1f5737b780b7a126e425e6ee393
Marie Otaki <otakimarie@gmail.com>
2020-05-02 13:00:39 -0700

    add sqrt

---
+++ sqrt/js/main.js
@@ -0,0 +1,16 @@
+var mySqrt = function(x) {
+  const sqrt = Math.sqrt(x);
+  // console.log(sqrt);
+
+  const result = Math.floor(sqrt);
+  // console.log(result);
+
+  return result;
+};
+
+mySqrt(9);
+
+// console.log(mySqrt(9));
+// console.log(mySqrt(4));
+// console.log(mySqrt(7));
+// console.log(mySqrt(6));
